d713edbfe218e03598b51a3495fac9a5580a87cf
MrNeznamy <124156688+MrNeznamy@users.noreply.github.com>
2025-10-20 21:41:11 +0200

    Small Changes

--- src/main/java/io/github/thebusybiscuit/slimefun4/api/events/PlayerRightClickEvent.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/api/events/PlayerRightClickEvent.java
@@ -16,7 +16,7 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 
-import eu.mrneznamy.mrlibcore.data.TriStateOptional;
+import eu.mrneznamy.slimefun5.data.TriStateOptional;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
 
 import me.mrCookieSlime.Slimefun.api.BlockStorage;
